049196c45781844e5fc42e0b07c90f3e1e55ff25
Mert Kutay Sezer <mkutaysezer@gmail.com>
2021-05-29 18:04:40 +0300

    cors origin whitelist added

--- backend/source/app/settings.py
+++ backend/source/app/settings.py
@@ -60,6 +60,10 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ORIGIN_WHITELIST = (
+       'http://localhost:3000',
+
+)
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
